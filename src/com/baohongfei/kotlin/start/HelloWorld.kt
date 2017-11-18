cee83bc45802c71d6c1be6d3016e95f2e58a5288
hongfei.bhf <hongfei.bhf@alibaba-inc.com>
2017-11-19 02:02:45 +0800

    first commit

---
+++ src/com/baohongfei/kotlin/start/HelloWorld.kt
@@ -0,0 +1,18 @@
+package com.baohongfei.kotlin.start
+
+import java.util.function.Consumer
+
+fun main(args:Array<String>){
+    println("Hello World!")
+
+    val list: List<String> = listOf("string1","string2","string3")
+
+    for(str in list){
+        println(str)
+    }
+
+    list.forEach(Consumer { println(it) })
+
+    list.forEach(System.out::println)
+
+}
